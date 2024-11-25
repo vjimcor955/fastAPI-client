08595010e15872d2046fc622341d8802ef6d2e14
vjimcor955 <vjimcor955@g.educaand.es>
2024-11-25 13:09:20 +0100

    Create HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,70 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+<script>
+export default {
+  name: "HomeView",
+  data() {
+    return {
+      user: "",
+      password: "",
+      token: "",
+    };
+  },
+  methods: {
+    async fetchUsers() {
+      const response = await fetch("http://localhost:8000/api/v1/items", {
+        headers: {
+          Authorization: `Bearer ${this.token}`,
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await response.json();
+    },
+  },
+};
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="home"></div>
 </template>
+
+<style lang="scss">
+.home {
+  min-height: inherit;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0 250px;
+  gap: 5%;
+  background-color: rgba(240, 46, 46, 0.05);
+
+  .title {
+    border: 1px solid black;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 75%;
+    width: 50%;
+    gap: 20px;
+    font-weight: bold;
+
+    .images {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      gap: 20px;
+
+      img {
+        width: 75px;
+      }
+    }
+
+    h1 {
+      font-size: 2rem;
+    }
+
+    p {
+      font-size: 1.2rem;
+    }
+  }
+}
+</style>
